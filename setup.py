da90f4eaaa2245bd0d059cf34b3b9e241405d8e1
loharprathmesh11 <loharprathmesh11@gmail.com>
2025-04-01 11:17:30 +0530

    project structure ready with proj1_venv

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Prathmesh Lohar",
+    author_email="loharprathmesh11@gmail.com",
+    packages=find_packages()
+)
